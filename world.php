12f658ad7472357867970a4ded304971932548fa
Kayla Swift <kayla.swift1302@gmail.com>
2022-11-13 20:55:44 -0500

    Added changes

--- world.php
+++ world.php
@@ -4,7 +4,7 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-
+//Get all the records from countries table
 $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -13,4 +13,23 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
   if (isset($_GET['country']) && !empty($_GET['country']) && !isset($_GET['context'])) {
     $cntry = ucwords(trim(filter_var($_GET["country"], FILTER_SANITIZE_STRING)));
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$cntry%' ");
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);}}  ?>
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC); }} ?>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Continent</th>
+        <th>Independence</th>
+        <th>Head of State</th>
+      </tr>
+      <tbody>
+        <?php foreach ($results as $row) : ?>
+          <tr>
+            <td><?= $row['name'] ?></td>
+            <td><?= $row['continent'] ?></td>
+            <td><?= $row['independence_year'] ?></td>
+            <td><?= $row['head_of_state'] ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  
